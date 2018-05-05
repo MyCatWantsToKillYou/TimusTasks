ecdfc0eef76247814980487e12983a6788b9514b
MyCatWantsToKillYou <mycatwantstokill@gmail.com>
2018-05-05 12:30:30 +0500

    Update .gitignore 2 tasks solved(1000,1001)

---
+++ Volume 1/1000/src/reverseRoot.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class reverseRoot {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        ArrayList <Double> roots = new ArrayList<>();
+        while(in.hasNext()){
+            long a = in.nextLong();
+            double root = Math.sqrt(a);
+            roots.add(root);
+        }
+        Collections.reverse(roots);
+        for(Double object:roots){
+            System.out.printf("%.4f\n",object);
+        }
+    }
+}
