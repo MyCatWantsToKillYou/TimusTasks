f2eae0f6f68530b277163b784c781bbe1fe5258f
MyCatWantsToKillYou <mycatwantstokill@gmail.com>
2018-11-20 21:26:03 +0500

    task solved(2023)

---
+++ Volume 11/src/Donald.py
@@ -0,0 +1,32 @@
+# task #2010
+# Difficulty 109
+
+position = 'left'
+steps = 0
+for i in range(int(input())):
+    name = input()
+    if position == 'center' and name[0] in ('A', 'P', 'O', 'R'):
+        position = 'left'
+        steps += 1
+    elif position == 'center' and name[0] in ('D', 'G', 'J', 'K', 'T', 'W'):
+        position = 'right'
+        steps += 1
+    elif position == 'center' and name[0] in ('B', 'M', 'S'):
+        continue
+    elif position == 'left' and name[0] in ('B', 'M', 'S'):
+        position = 'center'
+        steps += 1
+    elif position == 'left' and name[0] in ('D', 'G', 'J', 'K', 'T', 'W'):
+        position = 'right'
+        steps += 2
+    elif position == 'left' and name[0] in ('A', 'P', 'O', 'R'):
+        continue
+    elif position == 'right' and name[0] in ('B', 'M', 'S'):
+        position = 'center'
+        steps += 1
+    elif position == 'right' and name[0] in ('A', 'P', 'O', 'R'):
+        position = 'left'
+        steps += 2
+    else:
+        continue
+print(steps)
