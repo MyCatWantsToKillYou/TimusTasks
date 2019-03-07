f5b64afb892989f6d7d5378a4db9c54f88440055
pinigin_iv <pinigin_iv@taximaxim.ru>
2019-03-07 12:32:59 +0500

    task solved(2102)

---
+++ Volume 12/src/Cryptography.py
@@ -0,0 +1,39 @@
+n = int(input())
+if n < 1048576:
+    print('No')
+    exit()
+Ans = []
+count = 0
+ans = True
+
+for d in range(2, int(n**0.5)+1 and n+1):
+    while n % d == 0:
+        count += 1
+        n //= d
+    if count > 20:
+        ans = False
+        break
+    elif count == 20:
+        if n != 1:
+            ans = False
+        break
+    elif count == 19:
+        if (d+1)*(d+1) > n:
+            if d + 1 <= n:
+                break
+            else:
+                ans = False
+                break
+    if (d + 1)**(20-count) > n:
+        ans = False
+        break
+
+if ans and n != 1:
+    count += 1
+if count != 20:
+    ans = False
+
+if ans:
+    print('Yes')
+else:
+    print('No')
