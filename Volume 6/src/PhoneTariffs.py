82f512326d5d78f745bea717d07c38ff90f71fd7
pinigin_iv <pinigin_iv@taximaxim.ru>
2019-03-12 09:31:45 +0500

    task solved(1576)

--- Volume 6/src/PhoneTariffs.py
+++ Volume 6/src/PhoneTariffs.py
@@ -1,31 +1,21 @@
 # task #1576
 # Difficulty 144
 
-from datetime import timedelta, datetime
 
 N1, C1 = map(int, input().split())
 N2, T, C2 = map(int, input().split())
 N3 = int(input())
 calls = int(input())
-startdate = date = datetime(2019, 3, 5, 0, 0, 0)
-limit = startdate + timedelta(minutes=T)
+minutes = 0
 
 for i in range(calls):
-   a = datetime.strptime(input(), "%M:%S")
-   delta = timedelta(minutes=a.minute, seconds=a.second)
-   if delta > timedelta(seconds=6):
-      if delta.seconds % 60 != 0:
-         delta = timedelta(minutes=a.minute+1, seconds=0)
-         date += delta
-      else:
-         date += delta
+   mm, ss = map(int, input().split(':'))
+   if mm == 0 and ss <= 6:
+      continue
+   minutes += mm
+   if ss != 0:
+      minutes += 1
 
-if date.hour != 0:
-   minutes = date.hour*60 + date.minute
-   print('Basic:     ' + str(date.minute * C1 + N1))
-   print('Combined:  ' + str(minutes * C2 + N2))
-   print('Unlimited: ' + str(N3))
-else:
-   print('Basic:     ' + str(date.minute * C1 + N1))
-   print('Combined:  ' + str(max([0, date.minute - T])*C2 + N2))
-   print('Unlimited: ' + str(N3))
+print('Basic:     ' + str(minutes * C1 + N1))
+print('Combined:  ' + str(max([0, minutes - T])*C2 + N2))
+print('Unlimited: ' + str(N3))
