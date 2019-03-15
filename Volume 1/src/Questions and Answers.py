f975c536cc192b01d651015c6a79d3030240d37e
pinigin_iv <pinigin_iv@taximaxim.ru>
2019-03-15 14:53:09 +0500

    task solved(1026)

---
+++ Volume 1/src/Questions and Answers.py
@@ -0,0 +1,16 @@
+# task #1026
+# Difficulty 147
+a = []
+b = []
+c = []
+for i in range(int(input())):
+  a.append(int(input()))
+a.sort()
+input()
+q = int(input())
+for m in range(q):
+    c.append(a[int(input())-1])
+for j in range(len(c)):
+    print(c[j])
+
+
